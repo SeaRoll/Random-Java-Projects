2eeec5f49ebccda5f73d006e1a5442f965441108
SeaRoll <yo.han.joo@edu.huddinge.se>
2017-03-26 23:24:45 +0200

    changes

--- RandomJava/1_SimpleSorting/src/simpleSorting.java
+++ RandomJava/1_SimpleSorting/src/simpleSorting.java
@@ -9,13 +9,33 @@ public class simpleSorting {
 
     public static ArrayList<Integer> theList = new ArrayList();
     public static Scanner userInput = new Scanner(System.in);
-    private boolean repeat = true;
 
-    public static void main() {
-        do {
-            System.out.println("WELCOME\n\n1. Add Value\n2. Reset List\n3. Sort the list\n4. Quit");
+    public static void main() { //Starts here
+        Menu();
+    }
 
+    public static void resetScreen() { //Making sure to "reset" screen. Making things better
+        for(int i = 0; i < 99; i++) {
+            System.out.println();
+        }
+    }
 
-        }while(repeat);
+    public static void Menu() { //The menu class.
+        resetScreen();
+        System.out.println("WELCOME\n\n1. Add Value\n2. Reset List\n3. Sort the list\n4. Quit");
+        int chosen = userInput.nextInt();
+
+    }
+
+    public static void addToList() { //Add Value to list
+        resetScreen();
+        System.out.print("Integer Value to add: "); int theValue = userInput.nextInt();
+        theList.add(theValue);
+        Menu();
+    }
+
+    public static void clearList() { //Clear value
+        theList.clear();
+        Menu();
     }
 }
