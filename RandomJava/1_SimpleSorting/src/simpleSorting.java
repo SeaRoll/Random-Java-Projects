244cb8053d985f629628419ea89c0d5ac7722241
SeaRoll <yo.han.joo@edu.huddinge.se>
2017-03-26 23:18:26 +0200

    Sorting Array

---
+++ RandomJava/1_SimpleSorting/src/simpleSorting.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Uses Arraylist. sorts from the smallest to biggest number.
+ * Created by Yohan on 3/26/2017.
+ */
+public class simpleSorting {
+
+    public static ArrayList<Integer> theList = new ArrayList();
+    public static Scanner userInput = new Scanner(System.in);
+    private boolean repeat = true;
+
+    public static void main() {
+        do {
+            System.out.println("WELCOME\n\n1. Add Value\n2. Reset List\n3. Sort the list\n4. Quit");
+
+
+        }while(repeat);
+    }
+}
